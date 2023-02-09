bfc02197fbdee43244b9535c5a765121447db64e
taegyun <taegyun04@gmail.com>
2023-02-09 16:45:33 +0900

    Complete doudou_un first version

--- doudou_un.py
+++ doudou_un.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python3
-import sys, pdb
+import sys, pdb, datetime
 class ForkedPdb(pdb.Pdb):
     """A Pdb subclass that may be used
     from a forked multiprocessing child
@@ -23,13 +23,29 @@ class CountDownToEvent:
     
     def addDate(self, event_date):
         self.event_date = event_date
+        self.date_yy = int(self.event_date[:4])
+        self.date_mm = int(self.event_date[4:6])
+        self.date_dd = int(self.event_date[-2:])
 
     def writeData(self, file_name=None):
         self.file_name = f'{self.event_name}.txt'
+        self.f = open(self.file_name, 'w', encoding='utf8')
+        self.f.write(self.event_name)
+        self.f.write(str(self.event_date))
+    
+    def requestData(self):
+        d_day = datetime.date(year=self.date_yy, month=self.date_mm, day=self.date_dd)
+        days_delta = abs(datetime.date.today() - d_day)
+    
+        print(days_delta.days, 'days left until', self.event_name)
+
 
 ###
 
-# with open('event_list.txt', 'w+b', encoding='utf8'):
+# with open('event_list.txt', encoding='utf8'):
 
 ev = CountDownToEvent()
-ev.addEvent('정보처리기사 실기')
+ev.addEvent(input("Event name: "))
+ev.addDate(input("Event date (YYYYMMDD): "))
+ev.writeData("File name before '.txt' (optional): ")
+ev.requestData()
